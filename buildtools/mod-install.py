7a93caae56de210a24cb3c974d0036c097c4246d
tier940 <tsubasa.ito@relightings.com>
2022-10-19 19:21:39 +0900

    [update] rename buildtools

--- buildtools/mod-install.py
+++ buildtools/mod-install.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# GTE2 Server install
+# GTE2 Mod install
 
 ## Import library
 import json
@@ -18,15 +18,8 @@ headers = {
 ### Import List
 manifest_json = json.load(open("cf/manifest.json"))
 
-## Move dir
-shutil.move("./overrides/bansoukou/", "./")
-shutil.move("./overrides/config/", "./")
-shutil.move("./overrides/local/", "./")
-shutil.move("./overrides/resources/", "./")
-shutil.move("./overrides/scripts/", "./")
-
 ## Access CF and save file if request succeeds
-os.makedirs("mods")
+os.makedirs("./overrides/mods/")
 for key in manifest_json["files"]:
     mod_json = requests.get("{}/v1/mods/{}/files/{}".format(
         cf_url,
@@ -39,5 +32,5 @@ for key in manifest_json["files"]:
         mod_file = requests.get(mod_json["data"]["downloadUrl"], allow_redirects=True, stream=True, headers=headers)
         with open(mod_json["data"]["fileName"], "wb") as f:
             shutil.copyfileobj(mod_file.raw, f)
-            shutil.move(mod_json["data"]["fileName"], "./mods")
+            shutil.move(mod_json["data"]["fileName"], "./overrides/mods/")
             print("Downloaded {f}".format(f=mod_json["data"]["fileName"]))
