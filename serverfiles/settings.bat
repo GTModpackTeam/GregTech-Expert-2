10c6c0393dac348098c5fce8b717d2cc8bbdd8c0
tier940 <tsubasa.ito@relightings.com>
2023-02-20 23:22:17 +0900

    Add server launcher

---
+++ serverfiles/settings.bat
@@ -0,0 +1,9 @@
+REM Don't edit these values unless you know what you are doing.
+set INSTALL_JAR=forge-1.12.2-14.23.5.2860-installer.jar
+set SERVER_JAR=forge-1.12.2-14.23.5.2860.jar
+
+REM You can edit these values if you wish.
+set MIN_RAM=2048M
+set MAX_RAM=8192M
+set MAX_MetaspaceSize=512M
+set JAVA_PARAMETERS=-XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseNUMA -XX:+CMSParallelRemarkEnabled -XX:MaxTenuringThreshold=15 -XX:MaxGCPauseMillis=30 -XX:GCPauseIntervalMillis=150 -XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -XX:+UseBiasedLocking -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -Dfml.ignorePatchDiscrepancies=true -Dfml.ignoreInvalidMinecraftCertificates=true -XX:+UseFastAccessorMethods -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:+AggressiveOpts -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:SoftRefLRUPolicyMSPerMB=10000 -XX:ParallelGCThreads=10 -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2_112-116.xml
