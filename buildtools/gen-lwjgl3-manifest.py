2aca5f5916eedf2975f5d00a938300dfe5bb7e02
tier940 <tier940@protonmail.com>
2025-11-16 16:26:34 +0900

    fix cicd

--- buildtools/gen-lwjgl3-manifest.py
+++ buildtools/gen-lwjgl3-manifest.py
@@ -17,12 +17,12 @@ projectIDs = [
 ]
 
 # Read manifest.json
-with open('./manifest.json', 'r') as f:
+with open('../manifest.json', 'r') as f:
     data = json.load(f)
 
 # Remove lwjgl3 not supported mods
 data['files'] = [item for item in data['files'] if item['projectID'] not in projectIDs]
 
 # Write manifest.json
-with open('./cmmc/manifest.json', 'w') as f:
+with open('../cmmc/manifest.json', 'w') as f:
     json.dump(data, f, indent=2)
