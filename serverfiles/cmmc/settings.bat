1ca8a56175f06b2eb8a28d9d9a79d23b679361ec
tier940 <tier940@protonmail.com>
2026-04-13 19:40:47 +0900

    Fix CMMC Server boot

--- serverfiles/cmmc/settings.bat
+++ serverfiles/cmmc/settings.bat
@@ -1,9 +1,9 @@
 REM Don't edit these values unless you know what you are doing.
 set CLEANROOM_VERSION=CLEANROOMVERSION
 set INSTALL_JAR=cleanroom-%CLEANROOM_VERSION%-installer.jar
-set SERVER_JAR=cleanroom-%CLEANROOM_VERSION%-universal.jar
+set SERVER_JAR=cleanroom-%CLEANROOM_VERSION%.jar
 
 REM You can edit these values if you wish.
 set MIN_RAM=4096M
 set MAX_RAM=8192M
-set JAVA_PARAMETERS=-Dfile.encoding=UTF-8 -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150
+set JAVA_PARAMETERS=-Dfile.encoding=UTF-8 -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30
